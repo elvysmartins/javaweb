9e6934e1b02e75cb890a2386af05d843658c50de
elvysmartins <elvysmartins@hotmail.com>
2017-05-22 19:32:10 -0300

    CRUD na entidade Usuario

--- src/main/java/br/com/cursojava/presistencia/jdbc/UsuarioDAO.java
+++ src/main/java/br/com/cursojava/presistencia/jdbc/UsuarioDAO.java
@@ -1,6 +1,9 @@
 package br.com.cursojava.presistencia.jdbc;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.mysql.jdbc.Connection;
 import com.mysql.jdbc.PreparedStatement;
@@ -21,13 +24,12 @@ public class UsuarioDAO {
 			preparador.execute();
 			preparador.close();
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} 
 	}
 
 	public void alterar(Usuario usu) {
-		String sql = "update BancoJavaWeb.usuario set nome=?, login=?, senha=? where id=?";
+		String sql = "update BancoJavaWeb.usuario set nome=?, login=?, senha=md5(?) where id=?";
 		
 		try {
 			PreparedStatement preparador = (PreparedStatement) con.prepareStatement(sql);
@@ -41,7 +43,6 @@ public class UsuarioDAO {
 			preparador.close();
 			
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} 
 		
@@ -59,10 +60,159 @@ public class UsuarioDAO {
 			preparador.close();
 			
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} 
 		
 	}
+	
+	public void salvar (Usuario usu) {
+		if (usu.getId()!=null && usu.getId()!=0){
+			alterar(usu);
+		} else{
+			cadastrar(usu);
+		}
+	}
+	
+	public List<Usuario> buscarTodos(){
+		
+		String sql = "Select * from BancoJavaWeb.usuario order by id";
+		List<Usuario> lista = new ArrayList<Usuario>();
 
+		try {
+			PreparedStatement preparador = (PreparedStatement) con.prepareStatement(sql);
+			ResultSet resultado = preparador.executeQuery();
+			
+			while (resultado.next()){
+				Usuario usuario = new Usuario();
+				usuario.setId(resultado.getInt("id"));
+				usuario.setNome(resultado.getString("nome"));
+				usuario.setLogin(resultado.getString("login"));
+				usuario.setSenha(resultado.getString("senha"));
+				
+				lista.add(usuario);
+			}
+			
+            preparador.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return lista;	
+	}
+
+	public Usuario buscarPorId (Integer id){
+		
+		String sql = "Select * from BancoJavaWeb.usuario where id=?";
+		Usuario usuario = null;
+
+		try {
+			PreparedStatement preparador = (PreparedStatement) con.prepareStatement(sql);
+            preparador.setInt(1, id);
+            ResultSet resultado = preparador.executeQuery();
+
+            if (resultado.next()){
+            	usuario = new Usuario();
+    			usuario.setId(resultado.getInt("id"));
+    			usuario.setNome(resultado.getString("nome"));
+    			usuario.setLogin(resultado.getString("login"));
+    			usuario.setSenha(resultado.getString("senha"));            
+            }
+            
+            preparador.close();
+
+        } catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+        return usuario;
+        
+	}
+
+	public List<Usuario> buscarPorNome (String nome){
+		
+		String sql = "Select * from BancoJavaWeb.usuario where nome like ?";
+		List<Usuario> lista = new ArrayList<Usuario>();
+
+		try {
+			java.sql.PreparedStatement preparador = con.prepareStatement(sql);
+            preparador.setString(1, "%" + nome + "%");
+            ResultSet resultado = (ResultSet) preparador.executeQuery();
+
+            while (resultado.next()){
+            	Usuario usuario = new Usuario();
+    			usuario.setId(resultado.getInt("id"));
+    			usuario.setNome(resultado.getString("nome"));
+    			usuario.setLogin(resultado.getString("login"));
+    			usuario.setSenha(resultado.getString("senha"));
+    			lista.add(usuario);
+            }
+            preparador.close();
+            System.out.println("Consulta por Nome com sucesso!!!");
+        } catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+        return lista;
+        
+	}
+
+	/**
+	 * Busca login e senha do usuário
+	 * @param usuario - Objeto com login e senha a ser consultado
+	 * @return null quando não encontra no banco ou um ponteiro a um usuário quando encontra
+	 */
+	public Usuario autenticar (Usuario usuario){
+		
+		String sql = "Select * from BancoJavaWeb.usuario where login=? and senha =?";
+		Usuario usuarioRetorno = null;
+
+		try {
+			java.sql.PreparedStatement preparador = con.prepareStatement(sql);
+            preparador.setString(1, usuario.getLogin());
+            preparador.setString(2, usuario.getSenha());
+            ResultSet resultado = preparador.executeQuery();
+
+            if (resultado.next()){
+            	usuarioRetorno = new Usuario();
+            	usuarioRetorno.setId(resultado.getInt("id"));
+            	usuarioRetorno.setNome(resultado.getString("nome"));
+            	usuarioRetorno.setLogin(resultado.getString("login"));
+            	usuarioRetorno.setSenha(resultado.getString("senha"));            
+                System.out.println("Autenticação com sucesso!!!");
+            } else{
+            	System.out.println("Usuário não encontrado!!!");
+            }
+            
+            preparador.close();
+
+        } catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+        return usuarioRetorno;
+        
+	}
+
+	public Boolean existeUsuario (Usuario usuario){
+		
+		String sql = "Select * from BancoJavaWeb.usuario where login=? and senha =?";
+		boolean ret = false;
+
+		try {
+			java.sql.PreparedStatement preparador = con.prepareStatement(sql);
+            preparador.setString(1, usuario.getLogin());
+            preparador.setString(2, usuario.getSenha());
+            ResultSet resultado = preparador.executeQuery();
+            ret = resultado.next();
+            preparador.close();
+        } catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+        return ret;
+        
+	}
 }
--- src/test/java/br/com/cursojava/TesteUsuarioDAO.java
+++ src/test/java/br/com/cursojava/TesteUsuarioDAO.java
@@ -3,12 +3,16 @@ package br.com.cursojava;
 import br.com.cursojava.persistencia.entidade.Usuario;
 import br.com.cursojava.presistencia.jdbc.UsuarioDAO;
 
+
 public class TesteUsuarioDAO {
 
 	public static void main(String[] args) {
 		//testeCadastrar();
 		//testeAlterar();
-		testeExcluir();
+		//testeExcluir();
+		//testeSalvar();
+		//testeBuscarPorId();
+		testeBuscarTodos();
 	}
 
 	private static void testeCadastrar() {
@@ -45,6 +49,67 @@ public class TesteUsuarioDAO {
 		UsuarioDAO usuDAO = new UsuarioDAO();
 		usuDAO.excluir(usu);
 		System.out.println("Excluído com sucesso!");	
+	}
+
+	private static void testeSalvar() {
+		Usuario usu = new Usuario();
+		usu.setId(2);
+		usu.setNome("Nome alterado 2");
+		usu.setLogin("loginalterado 2");
+		usu.setSenha("senhanova2");
+		
+		UsuarioDAO usuDAO = new UsuarioDAO();
+		usuDAO.salvar(usu);
+		
+		System.out.println("Salvo com sucesso!");		
+
+		Usuario usunovo = new Usuario();
+		usunovo.setId(0);
+		usunovo.setNome("Nome incluido");
+		usunovo.setLogin("loginincluido");
+		usunovo.setSenha("senhaincluida");
+		
+		UsuarioDAO usuDAOnovo = new UsuarioDAO();
+		usuDAOnovo.salvar(usunovo);
+		
+		System.out.println("Salvo com sucesso!");		
+		
+	}
+	
+	private static void testeBuscarTodos() {
+		UsuarioDAO usuarioDAO = new UsuarioDAO();
+		java.util.List<Usuario> listaResultado = usuarioDAO.buscarTodos();
+
+		if (listaResultado.isEmpty()){
+			System.out.println("Não existem dados para listar!!!");	
+		} else{ 		
+			for (Usuario usuario: listaResultado){
+				System.out.println("Consulta todos com sucesso!!!");			
+				System.out.println(usuario);	
+
+			}
+		}
+		
+	}	
+	
+	private static void testeAutenticar () {
+		Usuario usuario = new Usuario();
+		usuario.setLogin("elvynho");
+		usuario.setSenha("123");
+		
+		UsuarioDAO usuarioDAO = new UsuarioDAO();
+    	System.out.println(usuarioDAO.autenticar(usuario));
+	}
+
+	private static void testeBuscarPorId() {
+		UsuarioDAO usuarioDAO = new UsuarioDAO();
+		Usuario usuario = usuarioDAO.buscarPorId(6);
+		if  (usuario != null){
+			System.out.println(usuario);	
+		} else{
+			System.out.println("Id não encontrado");
+		}
+		
 	}
 
 }
